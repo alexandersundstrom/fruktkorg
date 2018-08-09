7ec192372fcebae8f5e35e6b26d272b6a9a7a278
Mio Engel <mioengel@gmail.com>
2018-08-09 13:31:08 +0200

    added prod persistence.xml and more frukt test

--- persistence/src/test/java/com/evry/fruktkorgpersistence/FruktTest.java
+++ persistence/src/test/java/com/evry/fruktkorgpersistence/FruktTest.java
@@ -1,7 +1,87 @@
 package com.evry.fruktkorgpersistence;
 
+import com.evry.fruktkorgpersistence.model.Frukt;
+import com.evry.fruktkorgpersistence.model.Fruktkorg;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
 public class FruktTest {
 
+    private static FruktkorgService fruktkorgService;
+    private static FruktService fruktService;
 
+    @BeforeEach
+    public void init() {
+        EntityManager entityManager = Persistence.createEntityManagerFactory("test").createEntityManager();
 
+        FruktkorgDAO fruktkorgDAO = new FruktkorgDAOImpl();
+        fruktkorgDAO.setEntityManager(entityManager);
+
+        fruktkorgService = new FruktkorgServiceImpl(fruktkorgDAO);
+
+        FruktDAO fruktDAO = new FruktDAOImpl();
+        fruktDAO.setEntityManager(entityManager);
+
+        fruktService = new FruktServiceImpl(fruktDAO);
+    }
+
+    @Test
+    public void saveFruktAndRead() {
+        Fruktkorg fruktkorg = new Fruktkorg("Test Korg");
+        fruktkorgService.persist(fruktkorg);
+
+        Frukt frukt = new Frukt("äpple", 3, fruktkorg);
+
+        fruktService.persist(frukt);
+        fruktkorgService.refresh(fruktkorg);
+
+        Assertions.assertEquals(1, fruktService.listFrukt().size());
+        Assertions.assertEquals(1, fruktkorgService.listFruktkorg().size());
+        Assertions.assertEquals(1, fruktkorgService.listFruktkorg().get(0).getFruktList().size(), "Should be 1 frukt in fruktkorgen");
+    }
+
+    @Test
+    public void saveFruktAndReadEdit() {
+        Fruktkorg fruktkorg = new Fruktkorg("Test Korg");
+        fruktkorgService.persist(fruktkorg);
+
+        Frukt frukt = new Frukt("äpple", 3, fruktkorg);
+
+        fruktService.persist(frukt);
+
+        Assertions.assertEquals(1, fruktService.listFrukt().size());
+
+        frukt.setAmount(2);
+
+        fruktService.merge(frukt);
+
+        Assertions.assertEquals(1, fruktService.listFrukt().size());
+        Assertions.assertEquals(2, fruktService.listFrukt().get(0).getAmount());
+    }
+
+    @Test
+    public void moveFruktToAnotherFruktkorg() {
+        Fruktkorg fruktkorg = new Fruktkorg("Test Korg");
+        fruktkorgService.persist(fruktkorg);
+
+        Frukt frukt = new Frukt("äpple", 3, fruktkorg);
+
+        fruktService.persist(frukt);
+
+        Assertions.assertEquals(1, fruktService.listFrukt().size());
+
+        Fruktkorg fruktkorg2 = new Fruktkorg("Test Korg 2");
+        fruktkorgService.persist(fruktkorg2);
+
+        frukt.setFruktkorg(fruktkorg2);
+
+        fruktService.merge(frukt);
+
+        Assertions.assertEquals(1, fruktService.listFrukt().size());
+        Assertions.assertEquals(fruktkorg2.getId(), fruktService.listFrukt().get(0).getFruktkorg().getId());
+    }
 }
